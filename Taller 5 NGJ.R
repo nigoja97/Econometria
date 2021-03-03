98c392c449069eabcd996a61b2184661f8778e96
nigoja97 <nicolas.gonzalez07@est.uexternado.edu.co>
2021-03-03 15:57:36 -0500

    Punto 8

--- Taller 5 NGJ.R
+++ Taller 5 NGJ.R
@@ -3,44 +3,93 @@
 #Limpiar 
 rm(list = ls())
 
-#Base de datos
+#Base de datos y paquetes.
+install.packages("stargazer")
+library(stargazer)
+
 library(readxl)
-base_de_datos <- read_excel("growth.xlsx") #Suba la base de datos de la cual desea conocer la regresión lineal, en este caso en particular la base de datos en cuestión se llama growth.xlsx
+base_de_datos <- read_excel("growth.xlsx") #Suba la base de datos de la cual desea conocer la regresión lineal, en este caso en particular la base de datos en cuestión se llama growth.xlsx.
 View(base_de_datos)
 
-#1. Estimar la regresión lineal de la variable dependiente sobre un grupo de k variables independientes
+#1. Estimar la regresión lineal de la variable dependiente sobre un grupo de k variables independientes.
 #Se supone, como regla general, que la primera columna de la base de datos es la variable dependiente y el resto son independientes.
 
 var.dependiente <- as.matrix(base_de_datos[1])
-var.independientes <- as.matrix(base_de_datos[2:5]) #La regresion planteada pide que se tome como independiente hasta la variable 5 ("rev"). De lo contrario, var.independientes se definiría como  as.matrix(base_de_datos[2:dim(base_de_datos)[2]])                 
+var.independientes <- as.matrix(base_de_datos[2:5]) #La regresion planteada pide que se tome como independiente hasta la variable 5 ("rev"). De lo contrario, var.independientes se definiría como  as.matrix(base_de_datos[2:dim(base_de_datos)[2]]).                 
 
 mod1 <- lm(var.dependiente~var.independientes, data = base_de_datos)
 mod1
 mod1res <- stargazer(mod1, type="text")
 
-#2. Generar los valores ajustados de la variable dependiente
+#2. Generar los valores ajustados de la variable dependiente.
 coeficientes <- as.matrix(mod1$coefficients)
 
 intercepto <- matrix(1, nrow = dim(base_de_datos)[1], ncol = 1)
 observaciones <- cbind(intercepto, var.independientes)
 observaciones
 
-valores.ajustados <- valores%*%coeficientes
+valores.ajustados <- observaciones%*%coeficientes
 
-#3. Generar los residuales de la regresión
+#3. Generar los residuales de la regresión.
 u <- mod1$residuals
 
-#4. Generar los cuadrados de los residuales
+#4. Generar los cuadrados de los residuales.
 u2 <- u^2
 
-#5. Estimar la regresión auxiliar de los residuales al cuadrado sobre las variables independientes
-fit <- lm(u2 ~ var.independientes, data=base_de_datos) 
-R2 <- summary(fit)$r.squared
+#5. Estimar la regresión auxiliar de los residuales al cuadrado sobre las variables independientes.
+#H0: Homocedasticidad.
+fitbp <- lm(u2 ~ var.independientes, data=base_de_datos) 
+R2bp <- summary(fitbp)$r.squared
 k <- dim(var.independientes)[2]
-estadistico <- dim(base_de_datos)[1] * R2
-valorP <- pchisq(q=estadistico, df=k, lower.tail=FALSE)
-cbind(estadistico, valorP)
+n <- dim(base_de_datos)[1]
+estadisticobp <- n * R2bp
+valorPbp <- pchisq(q=estadisticobp, df=k, lower.tail=FALSE)
+Breusch_Pagan <- cbind(estadisticobp, valorPbp)
+Breusch_Pagan
+#No Rechaza H0.
 
-#6. 
+#6. Estimar la regresión auxiliar de los residuales al cuadrado sobre los valores ajustados de la variable dependiente y los cuadrados de los valores ajustados de la variable dependiente.
+#H0: Homocedasticidad.
+fitw <- lm(u2 ~ valores.ajustados+valores.ajustados^2, data=base_de_datos) 
+R2w <- summary(fitw)$r.squared
+estadisticow <- n * R2w
+valorPw <- pchisq(q=estadisticow, df=k, lower.tail=FALSE)
+White <- cbind(estadisticow, valorPw)
+White
+#No Rechaza H0
+
+#7. Para cada una de las regresiones estimadas en 5 y 6 calcular los estadísticos F y L.
+#Aunque no es necesario, volvemos a estimar k, n, R2bp y R2w. 
+k <- dim(var.independientes)[2]
+n <- dim(base_de_datos)[1]
+R2bp <- summary(fitbp)$r.squared
+R2w <- summary(fitw)$r.squared
+
+#Estimamos el estadistico F haciendo uso de la formula general.
+Fbp <- (R2bp/k)/((1-R2bp)/(n-(k+1)))
+Fw <- (R2w/k)/((1-R2w)/(n-(k+1)))
+Fbp
+Fw
+
+#Calculamos los valores P asociados al estadistico F de ambas pruebas.
+valorPbpF <- pf(q=Fbp, df1 = k, df2 = n-k-1, lower.tail = FALSE)
+valorPwF <- pf(q=Fw, df1 = k, df2 = n-k-1, lower.tail = FALSE)
+valorPbpF
+valorPwF
+#Bajo estos resultados podemos concluir que, NO se rechaza H0, por lo tanto, la prueba de Breusch-Pagan y de White determina que el modelo es homocedastico y no hay problemas de heterocedasticidad.
+
+#El estadistico L ya habia sido calculado en 5 y 6. Acá solo lo renombramos por comodidad.
+Lbp <- estadisticobp
+Lbw <- estadisticow
+
+#Del estadistico L ya conocemos los valores P que son los siguientes y los renombamos por comodidad.
+valorPbpL <- valorPbp
+valorPwL <- valorPw
+valorPbpL
+valorPwL
+#Bajo estos resultados podemos concluir que, NO se rechaza H0, por lo tanto, la prueba de Breusch-Pagan y de White determina que el modelo es homocedastico y no hay problemas de heterocedasticidad.
+
+#8. Generar errores estándar robustos a heterocedasticidad para cada uno de los estimadores de los parámetros del modelo.
+
 
 
