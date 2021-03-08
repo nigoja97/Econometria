a01afc4d015235f2a1b2e24215edfe91674d96d8
nigoja97 <nicolas.gonzalez07@est.uexternado.edu.co>
2021-03-08 14:24:35 -0500

    Final

--- Taller 5 NGJ.R
+++ Taller 5 NGJ.R
@@ -108,6 +108,7 @@ lmtest::coeftest(mod1, vcov. = sandwich::vcovHC(mod1, type = 'HC1'))
 #9. Regresión auxiliar para variación de la prueba White interacciones
 #Se estimó un modelo de white con interacciones para determinar la presencia de heterocedasticidad en el modelo.fitwint <- lm(u2 ~ var.independientes+(var.independientes%*%(t(var.independientes))), data=base_de_datos) 
 #H0: Homocedasticidad
+fitwint <- lm(u2 ~ var.independientes+(var.independientes%*%(t(var.independientes))), data=base_de_datos)
 R2wint <- summary(fitwint)$r.squared
 estadisticowint <- n * R2wint
 valorPwint <- pchisq(q=estadisticowint, df=k, lower.tail=FALSE)
@@ -116,6 +117,5 @@ Whiteint
 
 #A partir del resultado obtenido de la prueba de White con interacciones (Mismo resultado que en la prueba de Breusch-Pagan), se puede concluir que:
 #No se Rechaza H0
-
 
 
