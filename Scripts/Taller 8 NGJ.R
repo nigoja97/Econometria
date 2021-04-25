c9fc2cdc68f9c06d01d460d4680c927891be03f8
nigoja97 <nicolas.gonzalez07@est.uexternado.edu.co>
2021-04-25 11:50:11 -0500

    Taller 8

--- Scripts/Taller 8 NGJ.R
+++ Scripts/Taller 8 NGJ.R
@@ -1,7 +1,6 @@
 #############Taller 8##############----
-library(gdata)
+#PAQUETES
 library(readxl)
-library(Ecdat)
 library(rugarch)
 library(lubridate)
 library(tidyverse)
@@ -12,42 +11,39 @@ library(TSA)
 
 data <- read_excel("C:/Users/NIGOJ/Desktop/Nico/Cosas de la U/Econometria R/Econometria/Bases de datos/trm2020.xlsx")
 data$fecha <- ymd(data$fecha)
+data$TRM <- ts(data = data$TRM, frequency=365)
 
 #GRÁFICO DE LA TRM----
+#GRÁFICAS
+
+#TRM
 ggplot(data = data, mapping = aes(x = fecha)) + 
   geom_line(aes(y = TRM), color="darkblue")+
   scale_x_date(breaks = function(x) seq.Date(from = min(x),to = max(x),by = "24 months")) 
 
+
+#VARIACIÓN ABSOLUTA
 data[,"dTRM"] <- c(NA,diff(data$TRM))
 ggplot(data = data, mapping = aes(x = fecha)) + 
   geom_line(aes(y = dTRM), color="darkblue")+
   scale_x_date(breaks = function(x) seq.Date(from = min(x),to = max(x),by = "24 months"))
 
+
+#VARIACIÓN RELATIVA
 data[,"lndTRM"] <- c(NA,diff(log(data$TRM)))
 ggplot(data = data, mapping = aes(x = fecha)) + 
   geom_line(aes(y = lndTRM), color="darkblue")+
   scale_x_date(breaks = function(x) seq.Date(from = min(x),to = max(x),by = "24 months"))
 
-
 #ESTACIONARIEDAD----
-#Promedio movil de 7 y 30 dias para determinar componentes estacionarios y cíclicos de la serie
-data$Trmma7 = ma(data$TRM, order=7)
-data$Trmma30 = ma(data$TRM, order=30)
-
-ggplot() +
-  geom_line(data = data, aes(x = fecha, y = TRM, color = "TRM")) +
-  geom_line(data = data, aes(x = fecha, y = Trmma7,   color = "Semanal"))  +
-  geom_line(data = data, aes(x = fecha, y = Trmma30, color = "Mensual"))  +
-  ylab('TRM')+
-  scale_x_date(breaks = function(x) seq.Date(from = min(x),to = max(x),by = "24 months")) 
-
-
 #PRUEBAS ADF
 adf.test(data$TRM, alternative = "stationary")
 adf.test(diff(data$TRM), alternative = "stationary")
 adf.test(diff(log(data$TRM)), alternative = "stationary")
-#SE PUEDE CONLCUIR QUE LA SERIE ES INTEGRADA DE ORDEN 1 (d=1)
+#SE PUEDE CONLCUIR QUE LA SERIE TRM ES INTEGRADA DE ORDEN 1 (d=1)
 
+lndTRM <- diff(log(data$TRM))
+#EXTRAEMOS Y SEPARAMOS LA VARIACIÓN RELATIVA DE LA TRM
 
 #DETERMINACIÓN DE ÓRDEN ARMA(p,q)----
 #AUTOCORRELACION Y AUTOCORRELACION PARCIAL
@@ -59,11 +55,123 @@ pacf(data$TRM)
 
 par(mfrow=c(2,1))
 par(mar=c(2,4,1,1))
-
-acf(diff(data$TRM))
-pacf(diff(data$TRM))
-acf(diff(log(data$TRM)))
-pacf(diff(log(data$TRM)))
+acf(lndTRM,lag=30,tck=.02,xlab="",ylab="",main="")
+pacf(lndTRM,lag=30,tck=.02,xlab="",ylab="",main="")
 #AL TRABAJAR CON LA SERIE ESTACIONARIA PODEMOS DETERMINAR EL ORDEN AR(p) Y MA(q) DE MANERA ÓPTIMA
 
 #CREACIÓN DEL MODELO
+#POSIBLES ORDENES SEGUN AUTOCORRELACIONES
+#AR(p): 1, 8, 10, 13, 17, 18, 24, 26, 28
+#MA(q): 1, 8, 10, 13, 17, 19, 24, 28
+
+#MODELO 1: AR(1)
+spec.ar1 <- arfimaspec(mean.model=list(armaOrder=c(1,0),include.mean=TRUE))
+fit.ar1 <- arfimafit(spec=spec.ar1,data=lndTRM)
+fit.ar1
+res.ar1 = fit.ar1@fit$residuals
+Box.test(res.ar1,lag=8,type="Ljung-Box")
+Box.test(res.ar1,lag=9,type="Ljung-Box")
+Box.test(res.ar1,lag=13,type="Ljung-Box")
+
+#MODELO 2: AR(2)
+spec.ar2 <- arfimaspec(mean.model=list(armaOrder=c(2,0),include.mean=TRUE))
+fit.ar2 <- arfimafit(spec=spec.ar2,data=lndTRM)
+fit.ar2
+res.ar2 = fit.ar2@fit$residuals
+Box.test(res.ar2,lag=8,type="Ljung-Box")
+Box.test(res.ar2,lag=9,type="Ljung-Box")
+Box.test(res.ar2,lag=13,type="Ljung-Box")
+#COEFICIENTE AR2 NO SIGNIFICATIVO. MODELO 2 DESCARTADO
+
+#MODELO 3: ARMA(1,13(1,8,10,13))
+spec.arma113 <- arfimaspec(mean.model=list(armaOrder=c(1,13),include.mean=TRUE), 
+                         fixed.pars = list(ma2=0,ma3=0,ma4=0,ma5=0,ma6=0,ma7=0,
+                                           ma9=0,ma11=0,ma12=0))
+fit.arma113 <- arfimafit(spec=spec.arma113,data=lndTRM)
+fit.arma113
+res.arma113 = fit.arma113@fit$residuals
+Box.test(res.arma113,lag=8,type="Ljung-Box")
+Box.test(res.arma113,lag=16,type="Ljung-Box")
+Box.test(res.arma113,lag=28,type="Ljung-Box")
+#DIFERENTES COEFICIENTES NO SIGNIFICATIVOS PERO AR10 Y AR13 SIGNIFICATIVOS. DE TODOS MODOS, MODELO 3 DESCARTADO
+
+#MODELO 4: ARMA(1,28(10,13,17,24,28))
+spec.arma128 <- arfimaspec(mean.model=list(armaOrder=c(1,28),include.mean=FALSE), 
+                       fixed.pars = list(ma1=0,ma2=0,ma3=0,ma4=0,ma5=0,ma6=0,ma7=0,
+                                         ma8=0,ma9=0,ma11=0,ma12=0,ma14=0,ma15=0,ma16=0,
+                                         ma18=0,ma19=0,ma20=0,ma21=0,ma22=0,ma23=0,ma25=0,
+                                         ma26=0,ma27=0))
+
+fit.arma128 <- arfimafit(spec=spec.arma128,data=lndTRM)
+fit.arma128
+res.arma128 = fit.arma128@fit$residuals
+Box.test(res.arma128,lag=8,type="Ljung-Box")
+Box.test(res.arma128,lag=16,type="Ljung-Box")
+Box.test(res.arma128,lag=28,type="Ljung-Box")
+#TODOS LOS COEFICIENTES SON SIGNIFICATIVOS. MODELO 4 NO DESCARTADO
+
+infocriteria(fit.ar1)
+infocriteria(fit.ar2)
+infocriteria(fit.arma113)
+infocriteria(fit.arma128)
+#EL MODELO 4 ES EL QUE TIENE MEJOR COMPORTAMIENTO SEGÚN LOS CRITERIOS DE INFORMACIÓN. SE TOMARÁ ESTE MODELO PARA HACER EL PRONÓSTICO
+
+
+#INESTABILIDAD DE PARAMETROS----
+tm <- which(data$fecha=="2021-01-01")-1
+t <-  which(data$fecha=="2021-04-21")
+
+spec.arma128 <- arfimaspec(mean.model=list(armaOrder=c(1,28),include.mean=FALSE), 
+                           fixed.pars = list(ma1=0,ma2=0,ma3=0,ma4=0,ma5=0,ma6=0,ma7=0,
+                                             ma8=0,ma9=0,ma11=0,ma12=0,ma14=0,ma15=0,ma16=0,
+                                             ma18=0,ma19=0,ma20=0,ma21=0,ma22=0,ma23=0,ma25=0,
+                                             ma26=0,ma27=0))
+
+fit.arma128.pre <- arfimafit(spec=spec.arma128,data=lndTRM[1:tm], solver = "gosolnp")
+fit.arma128.pre
+fit.arma128.post <- arfimafit(spec=spec.arma128,data=lndTRM[(tm+1):length(lndTRM)], solver = "gosolnp")
+fit.arma128.post
+fit.arma128 <- arfimafit(spec=spec.arma128,data=lndTRM, solver = "gosolnp")
+fit.arma128
+
+length(fit.arma128@model$fixed.pars)    #   28: 10,13,17,24,28
+length(fit.arma128@fit$coef)            #  11: mu,ar1,ma10,...,ma28,sigma   
+n <- length(fit.arma128@fit$coef) - length(fit.arma128@model$fixed.pars)  - 1
+gl1 <- n
+gl2 <- t - (2*n)
+
+SSR1 = sum(fit.arma128.pre@fit$residuals^2)
+SSR2 = sum(fit.arma128.post@fit$residuals^2)
+SSR  = sum(fit.arma128@fit$residuals^2)
+Fcambio = ((SSR-SSR1-SSR2)/gl1)/((SSR1+SSR2)/gl2)
+pf(Fcambio,df1=5,df2=212-2*5,lower.tail = F)
+#NO HAY EVIDENCIA DE CAMBIO ESTRUCTURAL. P-VALOR 0.6727>0.05 ENTONCES NO SE RECHAZA H0
+
+
+#PRONÓSTICOS----
+which(data$fecha=="2021-01-01")
+fecha2021 = lndTRM[5844:length(lndTRM)]
+space = length(fecha2021)
+
+fore.arma128 = NULL
+for (i in 1:space) {
+  print(i)
+  fit.arma128 <- arfimafit(spec=spec.arma128,data=lndTRM[1:(5843+i-1)],solver="solnp")
+  fore.arma128[i] <- arfimaforecast(fit.arma128,n.ahead=1)@forecast$seriesFor
+}
+
+mean(fore.arma128)
+var(fore.arma128)
+par(mfrow=c(1,1))
+plot(fecha2021, type="l",col="red")
+lines(fore.arma128, type="l",col="blue")
+
+
+fore.error.arma128 = fore.arma128-fecha2021
+mean(fore.error.arma128)
+var(fore.error.arma128)
+
+mod4 <- summary(lm(fecha2021~fore.arma128))
+mod4
+
+
